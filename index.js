f314bffdba2fc75ae0840d98fbaf174323e21beb
jazzmam <ruta.kalytyte@gmail.com>
2020-11-24 08:33:07 +0100

    .

--- index.js
+++ index.js
@@ -24,10 +24,20 @@ function setNextQuestion() {
 
 function showQuestion(question) {
 	questionElement.innerText = question.questionText;
+	question.answers.forEach(answer => {
+		const button = document.createElement('button');
+		button.innerText = answer.text;
+		button.classList.add('btn');
+		if (answer.correct) {
+			button.dataset.correct = answer.correct;
+		}
+		button.addEventListener('click', selectAnswer);
+		answerButtonsElement.appendChild(button);
+	})
 }
 
 function selectAnswer() {
-
+	console.log("dfsfs");
 }
 
 const questions = [
